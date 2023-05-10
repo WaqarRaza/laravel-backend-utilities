95d9386922d73e02ff4285398beb41983474763e
waqarraza <waqarqadri6@gmail.com>
2023-05-11 00:52:29 +0500

    fixes

---
+++ src/lang/eo/response.php
@@ -0,0 +1,5 @@
+<?php [
+    "not_found" => "La respondo, kiun vi serĉas, ne trovita.",
+    "forbidden" => "Vi ne havas permeson fari ĉi tiun agon.",
+    "unauthenticated" => "Uzanto ne estas aŭtentikigita."
+];
---
+++ src/lang/es/response.php
@@ -0,0 +1,5 @@
+<?php [
+   "not_found" => "La respuesta que buscas no se encuentra.",
+   "forbidden" => "No tienes permiso para realizar esta acción.",
+   "unauthenticated" => "El usuario no está autenticado."
+];
